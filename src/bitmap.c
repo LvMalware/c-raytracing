2fcc9ab2fecc4d2c4901a80f452dae67a80af3c1
Lucas V. Araujo <lucas.vieira.ar@disroot.org>
2023-01-20 19:18:12 -0300

    added copyright notice

--- src/bitmap.c
+++ src/bitmap.c
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2023 Lucas V. Araujo (https://github.com/lvmalware)
+ *
+ * This file is part of c-raytracing.
+ * c-raytracing is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * c-raytracing is distributed in the hope that it will be useful,but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this file. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "bitmap.h"
 
 void bitmap_file_header_init(struct bitmap_file_header * const header, const uint32_t file_size) {
